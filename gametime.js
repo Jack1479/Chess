6f7ddabf5902515c0366099461144e75076848a2
Jack1479 <jackevans@efpost.com>
2025-09-23 11:22:04 +0000

    added js for pieces giving each piece a unique id

--- gametime.js
+++ gametime.js
@@ -1,13 +1,14 @@
 const pieceimg = document.getElementsByTagName('img')
 const allsqures = document.getElementsByClassName('square')
+const pieces = document.getElementsByClassName('piece')
 const rows = document.getElementById('row')
 const columns = document.getElementById('column')
 
+setup();
+setuppieces();
+
 function setup(){
     for(let i=0;1<allsqures.length;i++ ){
-    pieceimg[i].addEventListener('click' , allowclick)
-    pieceimg[i].addEventListener('place' , allowplace)
-    allsqures[i].addEventListener('click' , allowclick)
     allsqures[i].addEventListener('place' , allowplace)
     let row=8-math.floor(i/8)       /*adding row coordinates 1-8*/
     let column=String.fromCharCode(97+(i%8));     /*adding column coordinates a-h*/
@@ -15,6 +16,21 @@ function setup(){
     square.id=column+row;
 }}
 
-setup();
 
+function setuppieces(){
+    for(let i=0;1<pieces.length;i++ ){
+    pieces[i].addEventListener('click' , allowclick)
+    pieces[i].setAttribute('moveable', true)
+    pieces[i].id=pieces[i].classname.split(' ')[1];  /* giving each piece it name through taking the second part of its class name e.g.brook. for black rook*/
+}
+    for(let i=0;1<pieceimg.length;i++ ){
+        pieceimg.setAttribute('moveable', false)  /*making sure the pieces themselves are moving and not just the images*/
+    }}
 
+function allowtake(ev){    /* this function lets a piece be dropped on another piece, as by default it wont let you*/
+    ev.preventdefault()
+}
+
+function click(ev){
+    let selected = null;
+}
